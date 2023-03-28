39824323619b5c1868fab1cb49b1e67b8b4c04b4
ML <labrecquemegan@hotmail.com>
2023-03-28 19:58:53 -0400

    starting store front app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomepageComponent,
+    CartComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
